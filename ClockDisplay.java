6127579e841ab16aae14041cf51a74f8b3ddaeb5
adan291 <adanx05@gmail.com>
2015-11-23 09:08:57 +0100

    Creado metodo para elejir fecha y reloj por defecto o el de la propia maquina

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,10 +1,12 @@
 
+
 /**
  * Write a description of class ClockDisplay here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.*;
 public class ClockDisplay
 {
     //horas que asignas
@@ -15,16 +17,37 @@ public class ClockDisplay
     private String horaActual;
     //Atributo booleano para elejir tipo de reloj 12 horas o 24 horas
     private boolean tipos;
+    //Atributo booleano para elejir tipo de reloj Ordenador o defecto
+    private boolean calOrds;
+    //Los años asignar o hay
+    private NumberDisplay años;
+     //Los meses asignar o hay
+    private NumberDisplay meses;
+    //Los dias asignar o hay
+    private NumberDisplay dias;
+    
+    
+    
     /*
      * Constructor que inicializa a oo:oo
-     * Escribe true para formato 12 o false para formato 24
+     * Escribe true para elejir reloj del ordenador y false para el de por defecto
      */
-    public ClockDisplay(boolean tipo)
+    public ClockDisplay(boolean tipo, boolean calOrd )
     {
-       horas = new NumberDisplay(24);//Con esto creamos un nuevo objeto de la clase NumberDisplay
-       minutos = new NumberDisplay(59);//Con esto creamos un nuevo objeto de la clase NumberDisplay
-       actualHoraDisplay();
-       tipos = tipo;
+        calOrds = calOrd;
+        if (calOrd == true) {
+            Calendar c1 = GregorianCalendar.getInstance();
+            System.out.println("Fecha actual: "+c1.getTime().toLocaleString());
+        }
+        else{
+            horas = new NumberDisplay(24);//Con esto creamos un nuevo objeto de la clase NumberDisplay
+            minutos = new NumberDisplay(59);//Con esto creamos un nuevo objeto de la clase NumberDisplay
+            dias = new NumberDisplay(30);
+            meses = new NumberDisplay(12);
+            años = new NumberDisplay(99);
+            actualHoraDisplay();
+            tipos = tipo;
+        }
     }
     
     /*
@@ -38,22 +61,27 @@ public class ClockDisplay
                horaActual = hora + ":" + minutos.getDisplayValue() + " PM";
            }
            else{
-                horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " AM";
+                horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " AM" + " Cal " + dias.getDisplayValue() + "/" + meses.getDisplayValue() + "/" + años.getDisplayValue();
             }
        }     
        else{
-            horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+            horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue()+ " Cal " + dias.getDisplayValue() + "/" + meses.getDisplayValue() + "/" + años.getDisplayValue();
        }
     }
     
     /*
-     * Constructor para dar la hora y minutos dados
+     * Constructor para dar la hora  minutos  dia mes y año
+     * Escribe true para formato 12 o false para formato 24
+     * 
      */
-    public ClockDisplay(int hora, int minuto, boolean tipo)
+    public ClockDisplay(int hora, int minuto, int dia, int mes, int año, boolean tipo)
     {
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(59);
-        setTime(hora, minuto);
+        dias = new NumberDisplay(30);
+        meses = new NumberDisplay(12);
+        años = new NumberDisplay(99);
+        setTime(hora, minuto,dia,mes,año);
     }
     
     /*
@@ -65,12 +93,15 @@ public class ClockDisplay
     }
     
     /*
-     * Metodo poner hora fija sin devolver
+     * Metodo poner hora dia mes y año fija sin devolver
      */
-    public void setTime(int hora, int minuto)
+    public void setTime(int hora, int minuto, int dia, int mes, int año)
     {
         horas.setValue(hora);
         minutos.setValue(minuto);
+        dias.setValue(dia);
+        meses.setValue(mes);
+        años.setValue(año);
         actualHoraDisplay();
     }
     
